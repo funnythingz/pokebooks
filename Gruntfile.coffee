b999d3b0cf07537e44ebb8c66aba8d7b23da5efd
funnythingz <hiroki@hackleberry.me>
2013-11-16 19:51:45 +0900

    iroiro

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -2,54 +2,20 @@ module.exports = (grunt)->
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json')
 
-    concat:
-      ddd:
-        src: ['src/ddd/**/*.js']
-        dest: 'build/pokedex.js'
-
-      test:
-        src: ['build/pokedex.js', 'tests/**/*.js']
-        dest: 'build/pokedex-test.js'
-
-      options:
-        separator: ';'
-
-    typescript:
-      base:
-        src: ['src/**/*.ts', 'tests/**/*.ts']
-
     compass:
       dist:
         options:
           config: 'config.rb'
 
     watch:
-      ts:
-        files: ['src/**/*.ts', 'tests/**/*.ts']
-        tasks: ['typescript', 'concat', 'clean', 'copy']
-        options:
-          atBegin: true
-
       css:
         files: ['sass/**/*.scss']
         tasks: ['compass']
         options:
           atBegin: true
-
-    clean: ['src/**/*.js', 'tests/**/*.js']
-
-    copy:
-      app:
-        src:  'build/pokedex.js'
-        dest: 'meteor/pokedex.js'
-
   })
 
-  grunt.loadNpmTasks('grunt-typescript')
-  grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-contrib-watch')
   grunt.loadNpmTasks('grunt-contrib-compass')
-  grunt.loadNpmTasks('grunt-contrib-clean')
-  grunt.loadNpmTasks('grunt-contrib-copy')
 
-  grunt.registerTask('default', ['typescript', 'concat', 'clean', 'compass', 'copy'])
+  grunt.registerTask('default', ['compass'])
---
+++ meteor/client/client.ts
@@ -0,0 +1,129 @@
+/// <reference path="../packages/typescript-libs/meteor.d.ts" />
+/// <reference path="../packages/typescript-libs/ironrouter.d.ts" />
+/// <reference path="../packages/typescript-libs/node.d.ts" />
+
+class NavActiveChecker {
+
+    constructor(private flag: boolean = false) {}
+
+    false() {
+        this.flag = false;
+    }
+
+    true() {
+        this.flag = true;
+    }
+
+    callFlag(): boolean {
+        return this.flag;
+    }
+
+}
+
+var homeActiveChecker = new NavActiveChecker(false);
+var aboutActiveChecker = new NavActiveChecker(false);
+
+Router.map(function() {
+
+    this.route('home', {
+        path: '/',
+        layoutTemplate: 'layout',
+        template: 'homeTpl',
+        yieldTemplates: {
+            'headerTpl': {to: 'header'},
+            'asideTpl':  {to: 'aside'},
+            'footerTpl': {to: 'footer'}
+        },
+
+        action: function() {
+            console.log('home');
+            homeActiveChecker.true();
+            aboutActiveChecker.false();
+            this.render();
+        },
+
+        data: {
+            active: 'home'
+        }
+
+    });
+
+    this.route('about', {
+        path: '/about',
+        layoutTemplate: 'layout',
+        template: 'aboutTpl',
+        yieldTemplates: {
+            'headerTpl': {to: 'header'},
+            'asideTpl':  {to: 'aside'},
+            'footerTpl': {to: 'footer'}
+        },
+
+        action: function() {
+            console.log('about');
+            homeActiveChecker.false();
+            aboutActiveChecker.true();
+            this.render();
+        },
+
+        data: {
+            active: 'about'
+        }
+
+    });
+
+});
+
+
+declare var ItemsController;
+
+Template['headerTpl'].helpers({
+
+    title: () => {
+        return "Pokebooks";
+    }
+
+});
+
+Template['asideTpl'].helpers({
+
+    homeActive: () => {
+        return homeActiveChecker.callFlag();
+    },
+
+    aboutActive: () => {
+        return aboutActiveChecker.callFlag();
+    }
+
+});
+
+Template['footerTpl'].helpers({
+
+    copyright: () => {
+        return "funnythingz";
+    }
+
+});
+
+Template['homeTpl'].helpers({
+
+    title: () => {
+        return "Home";
+    },
+
+    description: () => {
+        return "This is `my pokebooks`.";
+    }
+
+});
+
+Template['aboutTpl'].helpers({
+
+    title: () => {
+        return "About";
+    },
+
+    description: () => {
+        return "This is `my pokebooks`.";
+    }
+
+});
