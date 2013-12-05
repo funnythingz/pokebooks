1db15d8028a78c599cebfb183623f2ef30dad19a
funnythingz <hiroki@hackleberry.me>
2013-12-05 13:19:40 +0900

    removed server dir

--- meteor/server/server.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../packages/typescript-libs/meteor.d.ts" />
-/// <reference path="../packages/typescript-libs/ironrouter.d.ts" />
-/// <reference path="../packages/typescript-libs/node.d.ts" />
-
-Meteor.startup(function () {
-});
